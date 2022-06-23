d82aca772f1e2638911cd792517d878f72f944a0
MrKomugiko <Mr.Komugiko@gmail.com>
2022-06-23 14:18:09 +0200

    Dodaj pliki projektów.

---
+++ KomugikoBot/KomugikoBot/Modules/InteractionModule.cs
@@ -0,0 +1,15 @@
+﻿using Discord.Interactions;
+using Discord;
+
+
+namespace KomugikoBot.Modules
+{
+    public class InteractionModule : InteractionModuleBase<SocketInteractionContext>
+    {
+        [SlashCommand("ping","Recieve a ping message!")]
+        public async Task HandlePingCommand()
+        {
+            await RespondAsync("Pong!");
+        }
+    }
+}
---
+++ KomugikoBot/KomugikoBot/Program.cs
@@ -0,0 +1,66 @@
+﻿using Discord;
+using Discord.Commands;
+using Discord.Interactions;
+using Discord.WebSocket;
+using KomugikoBot;
+using KomugikoBot.Modules;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+public class Program
+{
+    // program entry point
+    public static Task Main() => new Program().MainAsync();
+
+    public async Task MainAsync()
+    {
+        var config = new ConfigurationBuilder()
+            .SetBasePath(AppContext.BaseDirectory)
+            .AddJsonFile("config.json")
+            .Build();
+
+        using IHost host = Host.CreateDefaultBuilder()
+            .ConfigureServices((_, services) => services
+                .AddSingleton(config)
+                .AddSingleton(x => new DiscordSocketClient( new DiscordSocketConfig {
+                        GatewayIntents = Discord.GatewayIntents.AllUnprivileged,
+                        AlwaysDownloadUsers = true
+                    }))
+                .AddSingleton(x=>new InteractionService(x.GetRequiredService<DiscordSocketClient>()))
+                .AddSingleton<InteractionHandler>()
+                .AddSingleton(x=> new CommandService())
+                .AddSingleton<PrefixHandler>()
+                )
+            .Build();
+
+        await RunAsync(host);
+    }
+
+    public async Task RunAsync(IHost host)
+    {
+        using IServiceScope serviceScope = host.Services.CreateScope();
+        IServiceProvider provider = serviceScope.ServiceProvider;
+
+        var _client = provider.GetRequiredService<DiscordSocketClient>();
+        var sCommand = provider.GetRequiredService<InteractionService>();
+        await provider.GetRequiredService<InteractionHandler>().IntitializeAsync();
+        var config = provider.GetRequiredService<IConfigurationRoot>();
+        var pCommand = provider.GetRequiredService<PrefixHandler>();
+        pCommand.AddModule<PrefixModule>();
+        await pCommand.IntitializeAsync();
+
+        _client.Log += async (LogMessage msg) => { Console.WriteLine(msg.Message); };
+        sCommand.Log += async (LogMessage msg) => { Console.WriteLine(msg.Message); };
+       
+        _client.Ready += async () => {
+            Console.WriteLine("Bot ready!");
+            await sCommand.RegisterCommandsToGuildAsync(UInt64.Parse(config["guild:samuraje"]));
+        };
+        
+        await _client.LoginAsync(TokenType.Bot, config["token:samuraje"]);
+        await _client.StartAsync();
+
+        await Task.Delay(-1); // infinite loop, listening never ends
+    }
+}
